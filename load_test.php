3220d2fedc0a87f3d57afcdcf08f5aa19b7a2416
mogimogiko <moemmmoe66@gmail.com>
2024-06-20 18:16:08 +0900

    エラーを追加

---
+++ load_test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    class LoadTest {
+      public function test() {
+        echo 'ロードに成功しました。';
+      }
+    }
+    ?>
+  </p>
+</body>
+</html>
